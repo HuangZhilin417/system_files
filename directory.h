2d4383f41756f9acb9c205e44453a2e6b61a0202
HuangZhilin417 <sps263178@gmail.com>
2020-04-12 20:40:18 -0400

    added comment to directory.h

--- directory.h
+++ directory.h
@@ -16,12 +16,26 @@ typedef struct dirent {
     char _reserved[8];
 } dirent;
 
+//init the root dir, which is inode 0;
 void directory_init();
+
+//given a inode, which is a directory, it will search through the directory and see if that name exist
 int directory_lookup(inode* dd, const char* name);
+
+//given a path, it will look through the path given, and see if that file exist, if it does it will return the inode number for the last item in the path
 int tree_lookup(const char* path);
+
+//given a inode, which is the directory, it will put the name of the file or directory with the inum to the the inode directory given, and the is_dir indicates if the new file that is getting put in the inode directory is a file or a directory 
 int directory_put(inode* dd, const char* name, int inum, int is_dir);
+
+//deletes a file in the given inode directory, if refs become zero then that files data block also gets deleted
 int directory_delete(inode* dd, const char* name);
+
+//put the path into slist
 slist* directory_list(const char* path);
+
+//prints all the stuff in the directory and also prints nested
+//directory
 void print_directory(inode* dd);
 
 #endif
